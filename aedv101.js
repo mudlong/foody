854ce0ba408597a85134e1e17443a345dd6e8d6f
mudlong <38218449+mudlong@users.noreply.github.com>
2019-09-04 23:29:58 +0100

    Add files via upload

--- aedv101.js
+++ aedv101.js
@@ -32,7 +32,8 @@ const contractCode = `
 
 
 // const contractAddress = 'ct_7W4TBmvpHMpmp95stctgmBQoL9kqh6eLQUZNYRt7uHyiGdKUD';
-const contractAddress = 'ct_2VZeyiXtgLv3zGih9sGEnQMtzojRgcg9WYZtK4VQx7NfzBG2TA';
+// const contractAddress = 'ct_2VZeyiXtgLv3zGih9sGEnQMtzojRgcg9WYZtK4VQx7NfzBG2TA';
+const contractAddress = 'ct_SYi6Xg78ZE25vfQcyS1oZsR89zW6sYE2sezBYyTGkYUzuNCnc';
 
 
 var client = null;
