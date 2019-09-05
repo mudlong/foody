e0e82242e1a03913979b77abf492015937cd1761
mudlong <38218449+mudlong@users.noreply.github.com>
2019-09-05 13:54:38 +0100

    Add files via upload

--- aedv101.js
+++ aedv101.js
@@ -99,8 +99,7 @@ async function contractCall(func, args, value) {
 
 jQuery("#myDiv").on("click", ".buyFood", async function(event){
   $("#loader").show();
-  const value = $(this).value = parseInt("ize".value);
-  var loggy = this.parseInt("#ize".val())
+  const vague = parseInt(document.getElementById('ize').value);
   console.log(value)
   console.log(loggy)
   $("#loader").hide()
