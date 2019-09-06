ffacd9734ea9518bb99c570116865345551b84a9
mudlong <38218449+mudlong@users.noreply.github.com>
2019-09-06 13:28:52 +0100

    Add files via upload

--- aedv101.js
+++ aedv101.js
@@ -108,7 +108,7 @@ jQuery("#myDiv").on("click", ".buyFood", async function(event){
     var template =" {{ foodPrice }}" ;
       
 
-    var value = parseInt(Mustache.render(template, foodArray[2])); 
+    var value = parseInt(Mustache.render(template, foodArray[1])); 
     }   
   console.log(value)
       
